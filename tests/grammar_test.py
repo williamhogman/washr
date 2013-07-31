7440fc189cde8ae09f142714687c2b7a2bb76dd0
William Hogman <william@hogman.me>
2013-07-31 23:30:13 +0200

    added some simple tests for the grammar

---
+++ tests/grammar_test.py
@@ -0,0 +1,39 @@
+import pytest
+
+from washr.parser import grammar, types
+
+var_foo = types.Variable("foo", None)
+var_bar = types.Variable("bar", None)
+
+block_baz = types.BlockStart("baz")
+block_end_baz = types.BlockEnd("baz")
+
+parser_fixtures = {
+    "Normal string": ("foo", ["foo"]),
+    "String with {": ("{foo", ["{foo"]),
+    "Empty {} is not a var": ("{}", ["{}"]),
+    "Lone {": ("{", ["{"]),
+    "Lone }": ("}", ["}"]),
+    "Block: by itself": ("Block:", ["Block:"]),
+    "Lone :": (":", [":"]),
+    "Variable foo": ("{foo}", [var_foo]),
+    "Double wrapped in {}": ("{{foo}}", ["{", var_foo, "}"]),
+    "Asymmetric double wrapping": ("{{foo}", ["{", var_foo]),
+    "2 vars": ("{foo}{bar}", [var_foo, var_bar]),
+    "Open block": ("{block:baz}", [block_baz]),
+    "End block": ("{/block:baz}", [block_end_baz]),
+    "Inconsistent caps block": ("{bLocK:baz}", [block_baz]),
+    "Inconsistent caps block end": ("{/bLocK:baz}", [block_end_baz]),
+    "complete block": ("{Block:baz}{/block:baz}", [block_baz, block_end_baz]),
+    "complete block with contents": ("{Block:baz}foo{/Block:baz}", [block_baz, "foo", block_end_baz]),
+}
+
+parser_f_ids = parser_fixtures.keys()
+
+
+@pytest.mark.parametrize(("inp", "exp"), parser_fixtures.values(), ids=parser_f_ids)
+def test_grammar_quirks(inp, exp):
+    res = grammar.parse(inp)
+
+    for res_part, exp_part in zip(res, exp):
+        assert res_part == exp_part
