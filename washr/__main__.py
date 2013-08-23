a90bee546e04f5f526bf0fa461af3e062145b0ef
Niklas Korz <korz.niklask@gmail.com>
2013-08-23 21:59:46 +0200

    Block rendering, with loops etc

--- washr/__main__.py
+++ washr/__main__.py
@@ -3,11 +3,28 @@ from washr.template import Template
 
 def test():
     test = Template(
-        "{Block:Foo}Blawg  blawg {fooo} lol{PlaintextTitle}{/Block:Foo}"
+        """{Block:Foo}
+            Blawg  blawg {fooo} lol{PlaintextTitle}
+            {Block:Test}
+                <p>{Content}</p>
+            {/Block:Test}
+            {Block:Posts}
+                <h1>{Title}</h1>
+            {/Block:Posts}
+        {/Block:Foo}"""
     )
     print(test.render({
         "fooo": "Hello!",
-        "Title": "<h1>This totally is a test</h1>"
+        "Title": "<h1>This totally is a test</h1>",
+        "Foo": True,
+        "Test": {
+            "Content": "Hello guys!"
+        },
+        "Posts": [{
+            "Title": "An article"
+        }, {
+            "Title": "Another article"
+        }]
     }))
 
 
--- washr/template.py
+++ washr/template.py
@@ -1,24 +1,50 @@
 from washr.parser import ast, grammar
 from washr.transformations import transformation_table
 
+class State(object):
+    def __init__(self, ctx, parent=None):
+        self.ctx = ctx
+        self.parent = parent
+
+    def get(self, name, default=None):
+        if name in self.ctx:
+            return self.ctx[name]
+        if self.parent is not None and name in self.parent.ctx:
+            return self.parent.ctx[name]
+        return default
 
 class Template(object):
     def __init__(self, source):
         self._ast = ast.parse(grammar.parse(source))
 
-    def render(self, ctx, block=None):
+    def render(self, ctx={}, block=None, state=None):
+        if state is None:
+            state = State(ctx)
         if block is None:
             block = self._ast
         output = ""
         for n in block.children:
             if isinstance(n, ast.BlockNode):
-                output += self.render(ctx, n)
+                value = state.get(n.name)
+                if not value:
+                    continue
+                if isinstance(value, dict):
+                    output += self.render(block=n, state=State(value, state))
+                elif isinstance(value, list):
+                    output += ''.join([self.render(
+                        block=n, state=State(i, state)
+                    ) for i in value])
+                else:
+                    output += self.render(block=n, state=state) 
             elif isinstance(n, ast.VariableNode):
+                if not state.get(n.name):
+                    continue
+                value = str(state.get(n.name, ""))
                 if n.transformation is None:
-                    output += ctx.get(n.name, "")
+                    output += value
                 else:
                     transformator = transformation_table[n.transformation]
-                    output += transformator(ctx.get(n.name, ""))
+                    output += transformator(value)
             elif isinstance(n, ast.TextNode):
                 output += n.content
         return output
